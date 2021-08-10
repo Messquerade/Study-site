78fe58c0446d19fd5e2be6bf09e6e32ed160b25c
Messquerade <annac.klingberg@gmail.com>
2021-08-10 14:09:59 -0700

    build cards that toggle

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $("#JS").click(function () {
+    $("#JS-show").toggle();
+    $("#JS-hide").toggle();
+  });
+
+  $("#OP").click(function () {
+    $("#Operators-show").toggle();
+    $("#Operators-hide").toggle();
+  });
+
+  $("#JS-text").click(function () {
+    $("#JS-show").toggle();
+    $("#JS-hide").toggle();
+  });
+
+  $("#OP-text").click(function () {
+    $("#Operators-show").toggle();
+    $("#Operators-hide").toggle();
+  });
+});
